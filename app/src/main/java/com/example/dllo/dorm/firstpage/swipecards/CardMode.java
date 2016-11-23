fb336e87b0c4a1e68aa8d827dca501252b611590
zhaojun1217 <1334561398@qq.com>
2016-11-23 16:13:30 +0800

    把探探的页面效果做好  目前缺网络解析的数据  提交

---
+++ app/src/main/java/com/example/dllo/dorm/firstpage/swipecards/CardMode.java
@@ -0,0 +1,42 @@
+package com.example.dllo.dorm.firstpage.swipecards;
+
+import java.util.List;
+
+/**
+ * Created by zhaojun on 16/11/22.
+ */
+public class CardMode {
+    private String name;
+    private int year;
+    private List<String> images;
+
+    public CardMode(String name, int year, List<String> images) {
+        this.name = name;
+        this.year = year;
+        this.images = images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
